01d972409c85b041aba55e9fefd142b93e461fee
Ryan Riffle <ryanriffle@icloud.com>
2014-11-28 06:44:21 -0500

    Https

--- fonts.php
+++ fonts.php
@@ -2,7 +2,7 @@
     if ($_GET["family"]) {
         $data = file_get_contents ("http://www.fonts.googleapis.com/css?family=" . $_GET["family"]);
         if ($data == false) {
-            echo 'Could not load fonts.';
+            phpinfo();
             return;
         }
         return data;
